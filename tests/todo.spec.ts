6876f366b3ffecc59d69e2a3e565cad743c24cb6
Nitin Joy <nijoy@microsoft.com>
2025-07-14 12:49:59 +0530

    Revert "Fix test: tests/todo.spec.ts"
    This reverts commit c852c6b86aaeada9cc208a8a00749f44ff86fce4.

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -54,6 +54,7 @@ test("Frontend loads and basic functionality works not expected", async ({ page 
     console.log("Page structure loaded but API may not be working");
   }
   
-  // Removed intentional failure to fix the test
-  console.log("This test previously was designed to fail intentionally, but now passes.");
+  // Intentionally fail this test
+  console.log("This test is designed to fail intentionally");
+  await expect(page.locator("text=This Element Does Not Exist")).toBeVisible({ timeout: 5000 });
 });
